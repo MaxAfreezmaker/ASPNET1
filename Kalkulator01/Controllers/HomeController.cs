2e6c5a91deff51a4df6a49448bcc84f6dd7fdc4d
Maksymilian Adamski <adamskimaksymilian1@gmail.com>
2023-10-01 21:40:02 +0200

    Dokończenie kalkulatora

--- Kalkulator01/Controllers/HomeController.cs
+++ Kalkulator01/Controllers/HomeController.cs
@@ -36,10 +36,52 @@ namespace Kalkulator01.Controllers
             };
             return View(osoby);
         }
-        public IActionResult Urodziny( Urodziny urodziny) 
+        /*public IActionResult Urodziny( Urodziny urodziny) 
         {
             ViewBag.powitanie = $"Witaj {urodziny.Imie} masz {DateTime.Now.Year - urodziny.Rok}";
             return View(urodziny);
+        }*/
+        public IActionResult kalk(kalk model)
+        {
+            
+
+            int liczba1 = model.liczba;
+            int liczba2 = model.liczbadwa;
+            string znak = model.znak;
+
+            int wynik = 0;
+
+            switch (znak)
+            {
+                case "+":
+                    wynik = liczba1 + liczba2;
+                    break;
+                case "-":
+                    wynik = liczba1 - liczba2;
+                    break;
+                case "*":
+                    wynik = liczba1 * liczba2;
+                    break;
+                case "/":
+                    wynik = liczba1 / liczba2;
+                    if (liczba2 != 0)
+                    {
+                        wynik = liczba1 / liczba2;
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Druga liczba", "Nie można dzielić przez zero.");
+                        return View("kalk", model);
+                    }
+                    break;
+                default:
+                    ModelState.AddModelError("Znak", "Podałeś nie poprawny znak");
+                    break;
+            }
+            return View();
+            ViewBag.Wynik = wynik;
+            return View("Wynik");
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Kalkulator01/Models/kalk.cs
@@ -0,0 +1,9 @@
+﻿namespace Kalkulator01.Models
+{
+    public class kalk
+    {
+        public int liczba { get; set; }
+        public int liczbadwa { get; set; }
+        public string znak { get; set; }
+    }
+}
